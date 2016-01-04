c3311931d6080495197b4f65d2aacdf64ec9cdf9
mat-hek <mfront3@gmail.com>
2016-01-04 23:52:24 +0100

    changed clients' ports to dynamically allocated

--- src/main/scala/Chat/Client1.scala
+++ src/main/scala/Chat/Client1.scala
@@ -4,7 +4,13 @@ import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 
 object Client1 extends App {
-  val system = ActorSystem("Sys", ConfigFactory.load("client1.conf"))
-  val Jack = Client(system, "John","192.168.1.30:2553")
+  val port = "2553"
+  val config = ConfigFactory.parseString("include \"common\", akka.remote.netty.tcp.port = "++port)
+
+  val serverHostName = ConfigFactory.load("server.conf").getString("akka.remote.netty.tcp.hostname")
+  val serverPort = ConfigFactory.load("server.conf").getString("akka.remote.netty.tcp.port")
+  val system = ActorSystem("Sys", config)
+  val Jack = Client(system, "Jack", serverHostName ++":"++ serverPort)
   Jack ! Send("hello")
+
 }
--- src/main/scala/Chat/Client2.scala
+++ src/main/scala/Chat/Client2.scala
@@ -4,7 +4,12 @@ import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 
 object Client2 extends App {
-  val system = ActorSystem("Sys", ConfigFactory.load("client2.conf"))
-  val John = Client(system, "John","192.168.1.30:2553")
-  John ! Send("hi!")
+  val port = "2554"
+  val config = ConfigFactory.parseString("include \"common\", akka.remote.netty.tcp.port = "++port)
+
+  val serverHostName = ConfigFactory.load("server.conf").getString("akka.remote.netty.tcp.hostname")
+  val serverPort = ConfigFactory.load("server.conf").getString("akka.remote.netty.tcp.port")
+  val system = ActorSystem("Sys", config)
+  val John = Client(system, "John", serverHostName ++":"++ serverPort)
+  John ! Send("hi")
 }
