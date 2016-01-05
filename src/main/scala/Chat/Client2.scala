d5226495813044bf12223638745a09b5168d567e
mat-hek <mfront3@gmail.com>
2016-01-05 20:57:55 +0100

    created User class defining chat user who would be actor or server

--- src/main/scala/Chat/Client2.scala
+++ src/main/scala/Chat/Client2.scala
@@ -6,10 +6,17 @@ import com.typesafe.config.ConfigFactory
 object Client2 extends App {
   val port = "2554"
   val config = ConfigFactory.parseString("include \"common\", akka.remote.netty.tcp.port = "++port)
+  val system = ActorSystem("Sys", config)
+
+  val John = User("John",system)
 
   val serverHostName = ConfigFactory.load("server.conf").getString("akka.remote.netty.tcp.hostname")
   val serverPort = ConfigFactory.load("server.conf").getString("akka.remote.netty.tcp.port")
-  val system = ActorSystem("Sys", config)
-  val John = Client(system, "John", serverHostName ++":"++ serverPort)
-  John ! Send("hi")
+  val serverAddr = serverHostName ++":"++ serverPort
+
+  John ! BecomeClient(serverAddr)
+  Thread.sleep(2000)
+  println("sending hi")
+  John ! Send("Hi!")
+  John ! Send("Hi again!")
 }
