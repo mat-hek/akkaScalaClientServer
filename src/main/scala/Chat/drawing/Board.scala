e5c225e126519f73f3162a52ea3080f8e11742c1
mat-hek <mfront3@gmail.com>
2016-01-19 20:50:59 +0100

    Drawing fixes

--- src/main/scala/Chat/drawing/Board.scala
+++ src/main/scala/Chat/drawing/Board.scala
@@ -7,7 +7,7 @@ package Chat.drawing
 import scala.swing._
 import scala.swing.BorderPanel.Position._
 import event._
-import java.awt.{ Color, Graphics2D }
+import java.awt.{ Color, Graphics2D, BasicStroke }
 
 import scala.swing._
 object Board extends App
@@ -15,6 +15,7 @@ object Board extends App
   new Frame {
     visible = true
     title = "Board"
+    var lastPoint:Point = null
 
     val canvas = new Canvas {
       preferredSize = new Dimension(100, 100)
@@ -28,42 +29,50 @@ object Board extends App
     // specify which Components produce events of interest
     listenTo(canvas.mouse.clicks, canvas.mouse.moves)
 
+    var lineDrawingEvt = (point:Point) => {
+      val newPoint = new Point(point.x, point.y)
+      if (lastPoint == null)
+        lastPoint = newPoint
+      canvas.drawLine(new Line(lastPoint, newPoint, Color.green))
+      lastPoint = newPoint
+    }
     // react to events
     reactions += {
-      case MouseClicked(_, point, _, _, _) =>
-        canvas.drawPoint(new Point(point.x, point.y, Color.green))
-      case MouseDragged(_,point,_) =>
-        canvas.drawPoint(new Point(point.x, point.y, Color.green))
+      case MouseDragged(_,point,_) => lineDrawingEvt(point)
+      case MouseClicked(_,point,_,_,_) => lineDrawingEvt(point)
+      case _:MouseReleased | _:MouseExited => {
+        lastPoint = null
+      }
     }
   }
   println("board opened")
 }
 
 
-case class Point(val x: Int, val y: Int, val color: java.awt.Color)
+case class Line(val point1:Point, val point2:Point, val color: java.awt.Color)
 
 
 class Canvas extends Panel {
 
-  var points = List[Point]()
+  var lines = List[Line]()
 
   override def paintComponent(g: Graphics2D) {
 
     // Start by erasing this Canvas
+
     g.clearRect(0, 0, size.width, size.height)
 
-
     // Draw things that change on top of background
-    for (point <- points) {
-      g.setColor(point.color)
-      g.fillOval(point.x, point.y, 10, 10)
+    for (line <- lines) {
+      g.setColor(line.color)
+      g.setStroke(new BasicStroke(5))
+      g.drawLine(line.point1.x, line.point1.y, line.point2.x, line.point2.y)
     }
   }
 
-  /** Add a "dart" to list of things to display */
-  def drawPoint(point: Point) {
-    points = points :+ point
-    // Tell Scala that the display should be repainted
-    repaint()
+
+  def drawLine(line: Line): Unit = {
+    lines = lines :+ line
+    repaint
   }
 }
